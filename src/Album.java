256a580835639ac18472a02d70308c7937dabbd6
Sara T <sahara.tijol@gmail.com>
2020-10-19 11:25:30 -0500

    feat: Working on printTrackListing

--- src/Album.java
+++ src/Album.java
@@ -1,2 +1,35 @@
+import java.util.List;
+
 public class Album {
+    private String name;
+    private List<Song> songs;
+
+    // Constructors
+    public Album (String name, List<Song> songs) {
+        this.name = name;
+        this.songs = songs;
+    }
+
+    // Getters
+    public String getName() {
+        return this.name;
+    }
+
+    public List<Song> getSongs() {
+        return songs;
+    }
+
+    // Setters
+    public void setName() {
+        this.name = name;
+    }
+
+    public void setSongs() {
+        this.songs = songs;
+    }
+
+    // Print tracks
+    public void printTrackListings() {
+        System.out.println();
+    }
 }
